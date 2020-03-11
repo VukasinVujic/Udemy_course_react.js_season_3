944217dc72ce8668d303f585938fb514487cba7c
VukasinVujic <vukasinvujic021@gmail.com>
2020-03-11 10:49:24 +0100

    adding loading as Spinner

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
-
+import Spinner from './Spinner';
 
 class App extends React.Component {
     state = {
@@ -20,8 +20,7 @@ class App extends React.Component {
             if(this.state.lat && !this.state.errorMessage){
                 return <SeasonDisplay lat={this.state.lat}/>
             }
-            return <div>Loading</div>
-        
+            return <Spinner message="please accept location request"/> 
     }
 }
 
